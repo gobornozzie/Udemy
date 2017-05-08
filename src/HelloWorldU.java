3719efa7ccd33e46a3950dfe5ca03ec3af6e3678
Garry Oborn <Garry Oborn>
2017-05-08 17:14:59 -0600

    new repository made May 8, 2017

---
+++ src/HelloWorldU.java
@@ -0,0 +1,13 @@
+
+public class HelloWorldU {
+
+	public static void main(String[] args) {
+		/*
+		 * this is first Hello world method
+		 */
+		System.out.println("Hello World Udemy style May 8");	
+		System.out.println("This is the second line to this message 5/8/17");
+		
+	}
+
+}
