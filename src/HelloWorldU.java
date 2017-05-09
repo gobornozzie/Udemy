93743a1ae8fb76530caf16d64045580a7302a281
Garry Oborn <Garry Oborn>
2017-05-09 17:13:50 -0600

    new hello world line

--- src/HelloWorldU.java
+++ src/HelloWorldU.java
@@ -8,6 +8,7 @@ public class HelloWorldU {
 		System.out.println("Hello World Udemy style May 8");	
 		System.out.println("This is the second line to this message 5/8/17");
 		System.out.println("new line added");
+		System.out.println("Line added on 5/9/17");
 		
 	}
 
